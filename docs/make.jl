5562f48bbd154eae9800c7c351cf69b62cf7daf1
Daniel Silva <returndaniels@gmail.com>
2024-12-12 14:05:39 -0300

    Add documentation setup script for FastFE.jl using Documenter

---
+++ docs/make.jl
@@ -0,0 +1,23 @@
+using Documenter
+
+# Inclui o pacote que você deseja documentar
+using ..FastFE
+
+# Configurações do Documenter
+makedocs(
+    repo = "github.com/returndaniels/FastFE", # Repositório do GitHub
+    sitename = "Documentação FastFE.jl",  # Nome do site/documentação
+    modules = [FastFE],                    # Pacote a ser documentado
+    pages = [
+        "FastFE.jl" => "index.md",                 # Página principal da documentação
+        "SerialFiniteElements.jl" => "serial-finite-elements.md",
+        "VectorizedFiniteElements.jl" => "vectorized-finite-elements.md",
+        "SerialErrorAnalysis.jl" => "serial-error-analysis.md",
+        "VectorizedErrorAnalysis.jl" => "vectorized-error-analysis.md",
+        "MDEDiscretization.jl" => "mde-discretization.md",
+        "Benchmark.jl" => "benchmark.md",
+        "Gerar pacotes" => "generate-packages.md",
+        "Usando o Documenter.jl" => "using-documenter.md"
+    ],
+    format = Documenter.HTML()                # Você pode escolher o formato HTML, Markdown, ou LaTeX
+)
