64a5fa3c9b1cbf2a2f06cdeb8f2470c548a4abbd
Daniel Silva <returndaniels@gmail.com>
2024-12-12 11:39:50 -0300

    Refactor vectorized functions

--- src/VectorizedFiniteElements.jl
+++ src/VectorizedFiniteElements.jl
@@ -22,8 +22,19 @@ Calcula a matriz global de rigidez \( K \) para um sistema de elementos finitos.
 Retorna uma matriz esparsa do tipo `SparseMatrixCSC{Float64, Int64}`, que representa a matriz de rigidez global \( K \).
 """
 function K_vectorized(ne::Int64, m::Int64, h::Float64, npg::Int64,
-                      alpha::Float64, beta::Float64, gamma::Float64, EQoLG::Matrix{Int64})::SparseMatrixCSC{Float64, Int64}
-    # (função já definida anteriormente)
+    alpha::Float64, beta::Float64, gamma::Float64, EQoLG::Matrix{Int64})::SparseMatrixCSC{Float64, Int64}
+    @views begin
+        i = EQoLG[:, 1]
+        j = EQoLG[:, 2]
+        lin_idx = vcat(i, i, j, j)
+        col_idx = vcat(i, j, i, j)
+        k_values = vcat(fill(dot(W, 2*alpha*(dφ1P.*dφ1P)/h + gamma*(φ1P.*dφ1P) + beta*h*(φ1P.*φ1P)/2), ne),
+            fill(dot(W, 2*alpha*(dφ1P.*dφ2P)/h + gamma*(φ1P.*dφ2P) + beta*h*(φ1P.*φ2P)/2), ne),
+            fill(dot(W, 2*alpha*(dφ2P.*dφ1P)/h + gamma*(φ2P.*dφ1P) + beta*h*(φ2P.*φ1P)/2), ne),
+            fill(dot(W, 2*alpha*(dφ2P.*dφ2P)/h + gamma*(φ2P.*dφ2P) + beta*h*(φ2P.*φ2P)/2), ne))
+        K = sparse(lin_idx, col_idx, k_values, ne, ne)
+        return K[1:(ne-1),1:(ne-1)]
+    end
 end
 
 """
@@ -48,8 +59,16 @@ Calcula o vetor de forças externas \( F \) por meio de uma quadratura de Gauss,
 Retorna o vetor `F_ext_vectorized` preenchido com as contribuições das forças externas, calculadas pela quadratura de Gauss.
 """
 function F_vectorized!(F_ext_vectorized::Vector{Float64}, X::Matrix{Float64}, f_eval::Matrix{Float64}, values::Matrix{Float64},
-                       f::Function, ne::Int64, m::Int64, h::Float64, npg::Int64, EQoLG::Matrix{Int64})::Vector{Float64}
-    # (função já definida anteriormente)
+    f::Function, ne::Int64, m::Int64, h::Float64, npg::Int64, EQoLG::Matrix{Int64})::Vector{Float64}
+    @views begin
+        fill!(F_ext_vectorized, 0.0)
+        @. f_eval = f(X)
+        mul!(values, f_eval, WφP)
+        @simd for i in 1:2
+        F_ext_vectorized[EQoLG[:, i]] .+= values[:, i]
+        end
+            return (h/2)*F_ext_vectorized[1:ne-1]
+    end
 end
 
 """
@@ -73,8 +92,17 @@ Calcula o vetor de forças estendido \( G \) associado à função \( g \), util
 Retorna o vetor `G_ext_vectorized` preenchido com as contribuições das forças associadas à função \( g \), calculadas pela quadratura de Gauss.
 """
 function G_vectorized!(G_ext_vectorized::Vector{Float64}, g_eval::Matrix{Float64}, values::Matrix{Float64},
-                       C::Vector{Float64}, ne::Int64, m::Int64, h::Float64, npg::Int64, EQoLG::Matrix{Int64})::Vector{Float64}
-    # (função já definida anteriormente)
+    C::Vector{Float64}, ne::Int64, m::Int64, h::Float64, npg::Int64, EQoLG::Matrix{Int64})::Vector{Float64}
+    @views begin
+        fill!(G_ext_vectorized, 0.0)
+        C_ext = [C;0]
+        @. g_eval = g((φ1P')*C_ext[EQoLG[:, 1]] + (φ2P')*C_ext[EQoLG[:, 2]])
+        mul!(values, g_eval, WφP)
+        @simd for i in 1:2
+            G_ext_vectorized[EQoLG[:, i]] .+= values[:, i]
+        end
+        return (h/2)*G_ext_vectorized[1:(ne-1)]
+    end
 end
 
 # Exportando as funções para que sejam acessíveis externamente
