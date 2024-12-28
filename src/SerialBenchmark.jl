f5c768fd2f5ff1ca1f42a20b4c9d6ba227aff029
Leonardo Veiga <75226985+LeoVAF@users.noreply.github.com>
2024-12-28 17:07:40 -0300

    Add files via upload

--- src/SerialBenchmark.jl
+++ src/SerialBenchmark.jl
@@ -7,7 +7,19 @@ using FastGaussQuadrature
 using SparseArrays
 using DataFrames
 
-using BenchmarkTools # Usar @btime ou @benchmark na função que deseja avaliar
+using BenchmarkTools
+
+
+samples = 1000 # Quantidade de amostras para o benchmark de funções
+evals = 1 # Quantidade de avaliações por amostras para o benchmark de funções
+samples_sys = 10 # Quantidade de amostras para o benchmark do sistema linear
+evals_sys = 1 # Quantidade de avaliações por amostras para o benchmark do sistema linear
+# Recebe uma função para ser testada no benchmark
+function benchmark(func::Function, args::Any, samples::Int64; evals=1)
+    func(args...) # Compila a função antes de começar para ignorar tempo de compilação
+    resultado = @benchmark ($func($args...)) samples=samples evals=evals
+    return resultado
+end
 
 option = 1 # Opção de como calcula C0
 # Define as discretizações
@@ -19,19 +31,19 @@ x = h*(P .+ 1)/2 .+ a
 F_ext_serial = zeros(Float64, ne)
 G_ext_serial = zeros(Float64, ne);
 
-output_K = @benchmark K_serial(ne, m, h, npg, alpha, beta, gamma, EQoLG)
+output_K = benchmark(K_serial, (ne, m, h, npg, alpha, beta, gamma, EQoLG), samples, evals=evals)
 display(output_K)
 
-output_F = @benchmark F_serial!(F_ext_serial, x, x -> f(x, 0.0), ne, m, h, npg, EQoLG)
+output_F = benchmark(F_serial!, (F_ext_serial, x, x -> f(x, 0.0), ne, m, h, npg, EQoLG), samples, evals=evals)
 display(output_F)
 
-output_G = @benchmark G_serial!(G_ext_serial, C0, ne, m, h, npg, EQoLG)
+output_G = benchmark(G_serial!, (G_ext_serial, C0, ne, m, h, npg, EQoLG), samples, evals=evals)
 display(output_G)
 
 output_sys = @benchmark begin
     # Monta estrutura local global
     EQoLG = monta_EQ(ne)[monta_LG(ne)]
-
+    
     # Estruturas externas usadas pelas funções
     x = h*(P .+ 1)/2 .+ a
     F_ext_serial = zeros(Float64, ne)
@@ -52,11 +64,21 @@ output_sys = @benchmark begin
     C1 = LU_dec\(F_serial!(F_ext_serial, x, x -> f(x, tau*0.5), ne, m, h, npg, EQoLG) + MK*C0 -
          G_serial!(G_ext_serial, (C0 + C1_tiu)/2, ne, m, h, npg, EQoLG))
 
-    for n in 0:N
-        Cn = LU_dec\(F_serial!(F_ext_serial, x, x -> f(x, tau*(n-0.5)), ne, m, h, npg, EQoLG) + MK*C1 -
-             G_serial!(G_ext_serial, (3*C1 - C0)/2, ne, m, h, npg, EQoLG))
-        C0 = C1
-        C1 = Cn
+    # Pré-aloca memória para operações repetidas
+    Cn = zeros(Float64, ne-1)
+    G_C = zeros(Float64, ne-1)
+    MK_C1 = zeros(Float64, ne-1)
+    @views @simd for n in 2:N
+        # Cálculo sem consumo extra de memória
+        G_C .= 3*C1
+        G_C .-= C0
+        G_C ./= 2
+        MK_C1 .= MK*C1
+        ######################################
+        Cn .= LU_dec\(F_serial!(F_ext_serial, x, x -> f(x, tau*(n-0.5)), ne, m, h, npg, EQoLG) + MK_C1 -
+             G_serial!(G_ext_serial, G_C, ne, m, h, npg, EQoLG))
+        C0 .= C1
+        C1 .= Cn
     end
-end
+end samples=samples_sys evals=evals_sys
 display(output_sys)
--- src/VectorizedBenchmark.jl
+++ src/VectorizedBenchmark.jl
@@ -9,11 +9,22 @@ using DataFrames
 
 using BenchmarkTools # Usar @btime ou @benchmark na função que deseja avaliar
 
+samples = 1000 # Quantidade de amostras para o benchmark de funções
+evals = 1 # Quantidade de avaliações por amostras para o benchmark de funções
+samples_sys = 10 # Quantidade de amostras para o benchmark do sistema linear
+evals_sys = 1 # Quantidade de avaliações por amostras para o benchmark do sistema linear
+# Recebe uma função para ser testada no benchmark
+function benchmark(func::Function, args::Any, samples::Int64; evals=1)
+    func(args...) # Compila a função antes de começar para ignorar tempo de compilação
+    resultado = @benchmark ($func($args...)) samples=samples evals=evals
+    return resultado
+end
+
+option = 1 # Opção de como calcula C0
 # Monta estrutura local global
 EQoLG = monta_EQ(ne)[monta_LG(ne)]
-
 # Calcula o C0 inicial
-C0 = C0_options(1, u0, a, ne, m, h, npg, EQoLG)
+C0 = C0_options(option, u0, a, ne, m, h, npg, EQoLG)
 
 # Estruturas externas usadas pelas funções
 X = ((h/2)*(P .+ 1) .+ a)' .+ range(a, step=h, stop=b-h)
@@ -24,16 +35,16 @@ values = Matrix{Float64}(undef, ne, 2)
 F_ext_vectorized = zeros(Float64, ne)
 G_ext_vectorized = zeros(Float64, ne);
 
-output_K = @benchmark K_vectorized(ne, m, h, npg, alpha, beta, gamma, EQoLG)
+output_K = benchmark(K_vectorized, (ne, m, h, npg, alpha, beta, gamma, EQoLG), samples, evals=evals)
 display(output_K)
 
-output_F = @benchmark F_vectorized!(F_ext_vectorized, X, f_eval, values, x -> f(x, 0.0), ne, m, h, npg, EQoLG)
+output_F = benchmark(F_vectorized!, (F_ext_vectorized, X, f_eval, values, x -> f(x, 0.0), ne, m, h, npg, EQoLG), samples, evals=evals)
 display(output_F)
 
-output_G = @benchmark G_vectorized!(G_ext_vectorized, g_eval, values, C0, ne, m, h, npg, EQoLG)
+output_G = benchmark(G_vectorized!, (G_ext_vectorized, g_eval, values, C0, ne, m, h, npg, EQoLG), samples, evals=evals)
 display(output_G)
 
-output_sys =@benchmark begin
+output_sys = @benchmark begin
     # Monta estrutura local global
     EQoLG = monta_EQ(ne)[monta_LG(ne)]
     
@@ -61,12 +72,22 @@ output_sys =@benchmark begin
     C1 = LU_dec\(F_vectorized!(F_ext_vectorized, X, f_eval, values, x -> f(x, tau*0.5), ne, m, h, npg, EQoLG) + MK*C0 -
          G_vectorized!(G_ext_vectorized, g_eval, values, (C0 + C1_tiu)/2, ne, m, h, npg, EQoLG))
     
-    for n in 0:N
-       Cn = LU_dec\(F_vectorized!(F_ext_vectorized, X, f_eval, values, x -> f(x, tau*(n-0.5)), ne, m, h, npg, EQoLG) + MK*C1 -
-            G_vectorized!(G_ext_vectorized, g_eval, values, (3*C1 - C0)/2, ne, m, h, npg, EQoLG))
+    # Pré-aloca memória para operações repetidas
+    Cn = zeros(Float64, ne-1)
+    G_C = zeros(Float64, ne-1)
+    MK_C1 = zeros(Float64, ne-1)
+    @views @simd for n in 2:N
+        # Cálculo sem consumo extra de memória
+        G_C .= 3*C1
+        G_C .-= C0
+        G_C ./= 2
+        MK_C1 .= MK*C1
+        ######################################
+        Cn .= LU_dec\(F_vectorized!(F_ext_vectorized, X, f_eval, values, x -> f(x, tau*(n-0.5)), ne, m, h, npg, EQoLG) + MK*C1 -
+            G_vectorized!(G_ext_vectorized, g_eval, values, G_C, ne, m, h, npg, EQoLG))
         # Solução aproximada
-        C0 = C1
-        C1 = Cn
+        C0 .= C1
+        C1 .= Cn
     end
-end
+end samples=samples_sys evals=evals_sys
 display(output_sys)
